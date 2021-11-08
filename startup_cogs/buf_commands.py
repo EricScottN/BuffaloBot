34cfdc027239836fb6302a20c590606b0aab56e1
puhtron30 <eric.s.northup@gmail.com>
2021-11-08 12:06:09 -0500

    Will not post top 3 results

--- startup_cogs/buf_commands.py
+++ startup_cogs/buf_commands.py
@@ -1,7 +1,7 @@
 import os
 import googlemaps
 import discord
-from discord.ext import commands
+from discord.ext import commands, menus
 
 key = os.getenv("maps_api")
 
@@ -11,16 +11,27 @@ class BufCommands(commands.Cog, name='Buffalo Commands'):
         self.bot = bot
 
     @commands.command(name='wings',
-                      help='Get best wing locations for your area. Just pass your location!')
-    async def wings(self, ctx, *location):
+                      help='Get best wing locations for your area. Just pass your city or town! If no location is '
+                           'passed, will return results for Buffalo')
+    async def wings(self, ctx, location):
         if not location:
             location = 'Buffalo'
         gmaps = googlemaps.Client(key=key)
         wings_list = gmaps.places(f'wings in {location}, NY')['results']
+        if not wings_list:
+            return await ctx.send(f'Unable to find any wing spots in {location}')
         sorted_wings_list = sorted(wings_list, key=lambda d: d['rating'], reverse=True)
-        for business in sorted_wings_list:
-            pass
+        response = '\n**TOP FIVE RESTAURANTS**\n'
+        for x, business in enumerate(sorted_wings_list[:3], start=1):
+            if business['business_status'] != 'OPERATIONAL':
+                continue
+            response += f'```{x}.\n'
+            response += f"NAME: {business['name']}\n"
+            response += f"ADDRESS: {business['formatted_address']}\n"
+            response += f"RATING: {business['rating']}\n"
+            response += f"TOTAL RATINGS: {business['user_ratings_total']}```\n"
 
+        await ctx.send(response)
 
 def setup(bot):
     bot.add_cog(BufCommands(bot))
