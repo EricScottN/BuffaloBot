c9347a765753630dd2548ea32b1e275c4715a52d
puhtron30 <eric.s.northup@gmail.com>
2021-11-08 10:55:26 -0500

    bot works and wings list sorted by best result

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import googlemaps
 import discord
 from discord.ext import commands
@@ -6,22 +7,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix='buf', intents=intents)
+bot = commands.Bot(command_prefix='buf ', intents=intents)
 
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
-
-
-client = MyClient()
+@bot.event
+async def on_ready():
+    try:
+        print(f'{bot.user.name} has connected to Discord!')
+        for filename in os.listdir('./startup_cogs'):
+            if filename.endswith('.py'):
+                bot.load_extension(f'startup_cogs.{filename[:-3]}')
+        print('Start up cogs loaded successfully')
+    except Exception as e:
+        print(e)
 
 if __name__ == '__main__':
-    client.run(os.getenv('TOKEN'))
+    bot.run(os.getenv('TOKEN'))
 
--- startup_cogs/buf_commands.py
+++ startup_cogs/buf_commands.py
@@ -1,5 +1,6 @@
 import os
 import googlemaps
+import discord
 from discord.ext import commands
 
 key = os.getenv("maps_api")
@@ -11,9 +12,15 @@ class BufCommands(commands.Cog, name='Buffalo Commands'):
 
     @commands.command(name='wings',
                       help='Get best wing locations for your area. Just pass your location!')
-    async def wings(self, ctx, location):
+    async def wings(self, ctx, *location):
         if not location:
             location = 'Buffalo'
-
         gmaps = googlemaps.Client(key=key)
-        wings_list = gmaps.places(f'wings in {location}, NY')
+        wings_list = gmaps.places(f'wings in {location}, NY')['results']
+        sorted_wings_list = sorted(wings_list, key=lambda d: d['rating'], reverse=True)
+        for business in sorted_wings_list:
+            pass
+
+
+def setup(bot):
+    bot.add_cog(BufCommands(bot))
