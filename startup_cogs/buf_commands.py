414e16229265186e8239171e9f618554527cc5d7
puhtron30 <eric.s.northup@gmail.com>
2022-04-18 18:22:13 -0400

    snow works

--- startup_cogs/buf_commands.py
+++ startup_cogs/buf_commands.py
@@ -1,4 +1,6 @@
 import os
+import requests
+import json
 import googlemaps
 from discord.ext import commands
 
@@ -8,6 +10,21 @@ key = os.getenv("MAPS_API")
 class BufCommands(commands.Cog, name='Buffalo Commands'):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(name='snow')
+    async def snow(self, ctx, time=None):
+        if not time:
+            url = "https://api.weather.gov/stations/kbuf/observations/latest"
+            w = requests.get(url)
+            result = json.loads(w.content)['properties']
+            w.close()
+            snow = False
+            for present_weather in result['presentWeather']:
+                if 'snow' in present_weather['weather']:
+                    snow = True
+                    intensity = present_weather['intensity']
+                    return await ctx.send(f'It sure is! Currently there is {intensity} snow at the Buffalo Airport!')
+            return await ctx.send("Nope! No snow at the Buffalo Airport")
 
     @commands.command(name='wings',
                       help='Get best wing locations for your area. Just pass your city or town! If no location is '
