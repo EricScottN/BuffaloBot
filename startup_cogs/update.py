8ce207b374f0cd7456dc68a9b77ae892b381545f
EricScottN <eric.s.northup@gmail.com>
2022-12-27 09:40:48 -0500

    Save works with json

--- startup_cogs/update.py
+++ startup_cogs/update.py
@@ -1,5 +1,7 @@
-from typing import Dict, List
+from typing import Dict, List, Union, Optional, Sequence
 import logging
+import json
+import os
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -20,6 +22,12 @@ misc_delete_roles = [{"id": 719604297834299485, "name": "Local News"},
                      {"id": 719603773194109020, "name": "All Channels"}]
 change_roles = [{"id": 699934182822117396, "name": "Niagara Falls", "new_name": "Niagara"},
                 {"id": 699934215684489306, "name": "Expat", "new_name": "Just Visiting", "colour": "#eed8ff"}]
+exclude_cats = [{"id": 696096508910633000, "name": "Admin"}, {"id": 1028441474255310960, "name": "Real Ones"},
+                {"id": 1007627422667128932, "name": "Regulars"}, {"id": 696068936483209218, "name": "Voice Channels"},
+                {"id": 1041712180141232148, "name": "contest"}, {"id": 1029388594147762198, "name": "admin"},
+                {"id": 1037836509161717790, "name": "TOWNY MODS"},
+                {"id": 1021399801222397984, "name": "Friends of WNY"},
+                {"id": 1021399801222397985, "name": "USE YOUR WORDS"}]
 
 logger = logging.getLogger(__name__)
 
@@ -35,11 +43,10 @@ class Updater(commands.Cog, command_attrs=dict(hidden=True)):
 
         return commands.check(predicate)
 
-
     @commands.command(name="update")
     @commands.is_owner()
-    async def update(self, ctx):
-        print("updating")
+    async def update(self, ctx: commands.Context):
+        await ctx.message.delete()
         if ctx.guild.id == self.bot.testing_guild_id:
             await self.stage_testing_guild(ctx)
         else:
@@ -49,7 +56,7 @@ class Updater(commands.Cog, command_attrs=dict(hidden=True)):
         await self.replace_buffalo_roles(ctx)
 
         # delete all roles
-        await self.delete_all_roles(ctx)
+        await self.delete_all(ctx)
 
         # edit existing roles
         await self.edit_roles(ctx, change_roles)
@@ -57,13 +64,122 @@ class Updater(commands.Cog, command_attrs=dict(hidden=True)):
         # reset all role permissions to false
         await self.reset_all_perms(ctx)
 
+        # Create new categories
+        # Will need all role objects to set perms
+
     @commands.command(name="stage")
     @commands.is_owner()
     @is_testing_guild()
-    async def stage_testing_guild(self, ctx):
-        for role_set in [region_delete_roles, change_roles, misc_delete_roles]:
+    async def stage_testing_guild(self, ctx, *saves):
+        await ctx.message.delete()
+        if saves:
+            await self.save_json(ctx, saves)
+        role_sets = [region_delete_roles, change_roles, misc_delete_roles]
+        for role_set in role_sets:
             await self.create_roles(ctx, role_set)
         await self.assign_random_del_roles(ctx)
+        await self.copy_cat_and_channels(ctx)
+
+    async def save_json(self, ctx, saves):
+        discord_info = {}
+        file_path = "./files/discord_save.json"
+        if os.path.exists(file_path):
+            with open(file_path) as json_file:
+                discord_info = json.load(json_file)
+        buf_guild: discord.Guild = self.bot.get_guild(self.bot.bot_vars['BUFFALO_ID'])
+
+        save_mapping = {
+            "roles": buf_guild.roles,
+            "cats": buf_guild.categories,
+            "channels": buf_guild.channels
+        }
+
+        for save in saves:
+            if save not in save_mapping:
+                continue
+            discord_objects = save_mapping[save]
+            discord_info[save] = await self.get_discord_objects_info(ctx, discord_objects)
+
+        with open("./files/discord_info.json", "w") as j_file:
+            json.dump(discord_info, j_file, indent=1)
+
+    async def get_discord_objects_info(self, ctx, discord_objects):
+        result = []
+        for discord_object in discord_objects:
+            info = {
+                "id": discord_object.id,
+                "name": discord_object.name,
+                "exclude": await self.exclude(ctx, discord_object)
+            }
+            if isinstance(discord_object, discord.Role):
+                members = []
+                for member in discord_object.members:
+                    members.append({"id": member.id, "name": member.name})
+                info["members"] = members
+            result.append(info)
+        return result
+
+    def has_members_attribute(self, obj):
+        try:
+            obj.members
+            return True
+        except AttributeError:
+            return False
+
+    async def construct_discord_info(self, ctx, discord_info, save):
+        discord_info[save] = [
+            {"id": save.id,
+             "name": save.id,
+             "excluded": await self.exclude(ctx, save)}]
+        if save.members:
+            discord_info[save]["members"] = save.members
+
+    async def exclude(self, ctx, obj):
+        identifier = {"id": obj.id, "name": obj.name}
+
+        obj = await self.validate_exists(ctx, None, identifier)
+        return True if obj else False
+
+    @commands.command(name="backup")
+    @commands.is_owner()
+    @is_testing_guild()
+    async def backup_server(self, ctx: commands.Context):
+        backup = {"roles": [], "cats": [], "channels": []}
+        roles = ctx.guild.roles
+        for role in roles:
+            backup["roles"].append({"id": role.id, "name": role.name, "perms": role.permissions})
+
+    @commands.command(name="copy")
+    @commands.is_owner()
+    @is_testing_guild()
+    async def copy_cat_and_channels(self, ctx: commands.Context):
+        buf_cats: List[discord.CategoryChannel] = self.buffalo_guild.categories
+        for buf_cat in buf_cats:
+            if not any(exclude_cat['id'] == buf_cat.id for exclude_cat in exclude_cats):
+                overwrites = await self.prepare_overwrites(ctx, buf_cat)
+                new_category = await ctx.guild.create_category(
+                    name=buf_cat.name,
+                    overwrites=overwrites)
+                for channel in buf_cat.channels:
+                    overwrites = await self.prepare_overwrites(ctx, channel)
+                    if channel.type.name == "text":
+                        await ctx.guild.create_text_channel(name=channel.name,
+                                                            category=new_category,
+                                                            overwrites=overwrites)
+                    if channel.type.name == "forum":
+                        await ctx.guild.create_forum(name=channel.name,
+                                                     category=new_category,
+                                                     overwrites=overwrites)
+
+    async def prepare_overwrites(self,
+                                 ctx: commands.Context,
+                                 buf_cat: discord.CategoryChannel):
+        channel_overrides = {ctx.guild.default_role: buf_cat.overwrites_for(buf_cat.guild.default_role)}
+        for role, overwrites in buf_cat.overwrites.items():
+            ctx_role = await self.validate_exists(ctx, None, {"name": role.name})
+            if ctx_role:
+                channel_overrides[ctx_role] = overwrites
+        return channel_overrides
 
     async def create_roles(self, ctx, role_set: List):
         for role in role_set:
@@ -100,17 +216,21 @@ class Updater(commands.Cog, command_attrs=dict(hidden=True)):
         import random
         role = random.choice(region_delete_roles + misc_delete_roles)
         delete_role = role.get('role')
-        await self.validate_role_exists(ctx, delete_role, role)
+        await self.validate_exists(ctx, delete_role, role)
         await member.add_roles(role['role'])
 
-    async def validate_role_exists(self, ctx, validate_role, role):
+    async def validate_exists(self, ctx: commands.Context,
+                              validate_role: Optional[Union[None, discord.Object]],
+                              identifier: Dict):
         if not validate_role:
-            validate_role = await self.get_role_by_id_or_name(ctx, role)
+            validate_role = await self.get_by_id_or_name(ctx, identifier)
         return validate_role
 
     @staticmethod
-    async def get_role_by_id_or_name(ctx, role):
-        result = discord.utils.find(lambda r: r.id == role["id"] or r.name == role["name"], ctx.guild.roles)
+    async def get_by_id_or_name(ctx, identifier: Dict):
+        result = discord.utils.find(lambda r:
+                                    r.id == identifier.get("id") or
+                                    r.name == identifier.get("name"), ctx.guild.roles)
         return result
 
     async def check_replace_role(self, member):
@@ -125,7 +245,7 @@ class Updater(commands.Cog, command_attrs=dict(hidden=True)):
 
     async def get_roles_in_role_set(self, ctx, role_set: List[Dict]):
         for role in role_set:
-            await self.get_role_by_id_or_name(ctx, role)
+            await self.get_by_id_or_name(ctx, role)
 
     @commands.command(name="replace")
     @commands.is_owner()
@@ -139,7 +259,7 @@ class Updater(commands.Cog, command_attrs=dict(hidden=True)):
             await self.get_and_replace_role(ctx, role)
 
     async def get_and_replace_role(self, ctx, role):
-        add_role = role.get("role", await self.validate_role_exists(ctx, None, role))
+        add_role = role.get("role", await self.validate_exists(ctx, None, role))
         if add_role:
             await self.get_members_and_replace_roles(ctx)
 
@@ -151,10 +271,24 @@ class Updater(commands.Cog, command_attrs=dict(hidden=True)):
     @commands.command(name="delete")
     @commands.is_owner()
     @is_testing_guild()
+    async def delete_all(self, ctx, *discord_object_type):
+        await ctx.message.delete()
+        if discord_object_type == 'roles' or not discord_object_type:
+            await self.delete_all_roles(ctx)
+            print("Deleted all roles")
+        if discord_object_type == 'cats' or not discord_object_type:
+            await self.delete_all_cats(ctx)
+            print("Deleted all categories")
+
+    async def delete_all_cats(self, ctx):
+        for cat in ctx.guild.categories:
+            if not any(exclude_cat['id'] == cat.id for exclude_cat in exclude_cats):
+                await cat.delete()
+
     async def delete_all_roles(self, ctx):
         role_sets = [region_delete_roles, misc_delete_roles]
-        # if ctx.guild.id == self.bot.testing_guild_id:
-        #     role_sets += [new_roles, change_roles]
+        if ctx.guild.id == self.bot.testing_guild_id:
+            role_sets += [new_roles, change_roles]
         for role_set in role_sets:
             await self.delete_roles(ctx, role_set)
 
@@ -166,7 +300,7 @@ class Updater(commands.Cog, command_attrs=dict(hidden=True)):
             await self.get_and_del_role(ctx, role)
 
     async def get_and_del_role(self, ctx, role):
-        delete_role = role.get("role", await self.validate_role_exists(ctx, None, role))
+        delete_role = role.get("role", await self.validate_exists(ctx, None, role))
         if delete_role:
             await self.delete_del_role(delete_role, role)
 
@@ -180,7 +314,7 @@ class Updater(commands.Cog, command_attrs=dict(hidden=True)):
             await self.get_and_edit_role(ctx, role)
 
     async def get_and_edit_role(self, ctx, role):
-        edit_role = role.get('role', await self.validate_role_exists(ctx, None, role))
+        edit_role = role.get('role', await self.validate_exists(ctx, None, role))
         if edit_role:
             await edit_role.edit(name=role.get('new_name') if not None else role.get('name'))
 
