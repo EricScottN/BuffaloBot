90116d2720be7c2ce01e4c78474345f2102902e4
EricScottN <eric.s.northup@gmail.com>
2024-04-13 08:35:29 -0400

    init_db created as discord task that runs when bot is ready

--- db/db_init.py
+++ db/db_init.py
@@ -1,10 +1,11 @@
 import logging
 from discord.ext import tasks, commands
-from discord import Guild
-from db.models import Guild
-from db.utils import insert_guilds
+from db.models import Guild, Role, Region
+from db.utils import insert_guilds, insert_roles
 from buffalobot import BuffaloBot
 from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy import select
+from sqlalchemy.orm import load_only
 
 logger = logging.getLogger(__name__)
 
@@ -12,7 +13,6 @@ logger = logging.getLogger(__name__)
 class Database(commands.Cog):
     def __init__(self, bot: BuffaloBot) -> None:
         self.bot = bot
-        self.engine = self.bot.engine
         self.init_db.start()
 
     def cog_unload(self) -> None:
@@ -21,10 +21,31 @@ class Database(commands.Cog):
     @tasks.loop(seconds=60)
     async def init_db(self):
         await self.bot.wait_until_ready()
-        async_session = async_sessionmaker(self.engine, expire_on_commit=False)
-        async with async_session() as session:
-            await insert_guilds(session, self.bot.guilds)
-
+        for guild in self.bot.guilds:
+            guild_model = Guild(
+                id=guild.id,
+                name=guild.name
+            )
+            stmt = select(Region).options(load_only(Region.name))
+            regions = []
+            async with self.bot.session() as session:
+                result = await session.scalars(stmt).all()
+                for region in result:
+                    regions.append(region.name)
+            for role in guild.roles:
+                if role in regions:
+                    region = select(Region).where(Region.name == role)
+                guild_model.roles.append(
+                    Role(
+                        id=role.id,
+                        name=role.name,
+                        guild_id=guild.id
+                    )
+                )
+            async with self.bot.session.begin() as session:
+                await session.merge(guild_model)
+                await session.commit()
+        print("Done")
 
 async def setup(bot: BuffaloBot) -> None:
     await bot.add_cog(Database(bot))
