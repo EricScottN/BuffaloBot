51d1449672162355a1841a055ad7be43ac6c3096
EricScottN <eric.s.northup@gmail.com>
2024-04-14 20:28:00 -0400

    Changed tablenames to singular and added more role attrs

--- db/models.py
+++ db/models.py
@@ -45,12 +45,12 @@ region_int_pk = Annotated[int, mapped_column(Identity(), primary_key=True)]
 name_str = Annotated[str, mapped_column(String)]
 message_len = Annotated[int, mapped_column(SmallInteger)]
 discord_date = Annotated[datetime, mapped_column(Date)]
-guild_fk = Annotated[int, mapped_column(ForeignKey("guilds.id"))]
-role_fk = Annotated[int, mapped_column(ForeignKey("roles.id"))]
+guild_fk = Annotated[int, mapped_column(ForeignKey("region.id"))]
+role_fk = Annotated[int, mapped_column(ForeignKey("role.id"))]
 
 
 class Guild(Base):
-    __tablename__ = "guilds"
+    __tablename__ = "region"
 
     # Attributes
     id: Mapped[discord_id_pk]
@@ -62,13 +62,18 @@ class Guild(Base):
 
 
 class Role(Base):
-    __tablename__ = 'roles'
+    __tablename__ = 'role'
 
     # Attributes
     id: Mapped[discord_id_pk]
     name: Mapped[name_str]
     guild_id: Mapped[guild_fk]
+    color: Mapped[int]
+    display_icon: Mapped[str]
+    bot_managed: Mapped[bool]
+    position: Mapped[int]
 
+    region_id: Mapped[Optional[int]] = mapped_column(ForeignKey("region.id"))
     region: Mapped[Optional[Region]] = relationship(
         default=None,
         back_populates="role"
@@ -77,10 +82,13 @@ class Role(Base):
 
 
 class Region(Base):
-    __tablename__ = 'regions'
+    __tablename__ = 'region'
 
     id: Mapped[region_int_pk]
     name: Mapped[name_str]
 
-    role_id: Mapped[role_fk]
     role: Mapped[Role] = relationship(back_populates="region")
+
+class Channel(Base):
+    __tablename__ = 'channels'
+
