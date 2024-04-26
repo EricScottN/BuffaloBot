eff6b4ca27d9af63d1f21d60bc33ccb11d754db1
EricScottN <eric.s.northup@gmail.com>
2024-04-26 15:25:40 -0400

    Moved db_init.py code to discord_db.py because it wasn't init code

---
+++ startup_cogs/discord_db.py
@@ -0,0 +1,42 @@
+"""
+Module to initialize database
+"""
+
+import logging
+from discord.ext import tasks, commands
+from db.utils import refresh_db
+
+from buffalobot import BuffaloBot
+
+logger = logging.getLogger(__name__)
+
+
+class Database(commands.Cog):
+    """
+    Discord.py cog class
+    """
+
+    def __init__(self, bot: BuffaloBot) -> None:
+        self.bot = bot
+        self.refresh_db.start()
+
+    async def cog_unload(self) -> None:
+        """
+        Discord.py method for unloading Cog
+        """
+        self.refresh_db.stop()
+
+    @tasks.loop(seconds=60)
+    async def refresh_db(self):
+        """
+        Discord.py task to refresh database on init and every 24 hours
+        """
+        await self.bot.wait_until_ready()
+        await refresh_db(self.bot)
+
+
+async def setup(bot: BuffaloBot) -> None:
+    """
+    Discord.py setup function to initialize Cog
+    """
+    await bot.add_cog(Database(bot))
