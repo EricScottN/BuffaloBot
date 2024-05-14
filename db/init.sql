164e4bf49d69eca80d0f03527bf7e5d60824e1e2
EricScottN <eric.s.northup@gmail.com>
2024-05-14 08:21:24 -0400

    Updated init.sql to better reflect sqlalchemy models

--- db/init.sql
+++ db/init.sql
@@ -1,19 +1,23 @@
 BEGIN;
 
+CREATE SEQUENCE region_id_seq;
+
 CREATE TABLE IF NOT EXISTS public.region
 (
-    id serial NOT NULL,
-    name text COLLATE pg_catalog."default" NOT NULL,
+    id integer NOT NULL DEFAULT nextval('region_id_seq'::regclass),
+    name character varying COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT region_pk PRIMARY KEY (id)
 );
 
+CREATE SEQUENCE role_group_id_seq;
+
 CREATE TABLE IF NOT EXISTS public.role_group
 (
-    id serial NOT NULL,
-    name text COLLATE pg_catalog."default" NOT NULL,
+    id integer NOT NULL DEFAULT nextval('role_group_id_seq'::regclass),
+    name character varying COLLATE pg_catalog."default" NOT NULL,
     permission_value bigint NOT NULL,
     CONSTRAINT role_group_pk PRIMARY KEY (id),
-    CONSTRAINT role_group_name_key UNIQUE (name)
+    CONSTRAINT name_permissions_key UNIQUE (name, permission_value)
 );
 
 INSERT INTO public.region (name)
