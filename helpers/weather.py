9eb476231a8211f93ab872fb6f198fd038312d1a
EricScottN <eric.s.northup@gmail.com>
2024-05-21 15:51:41 -0400

    Fixed wx commands to use aiohttp and refactored

--- helpers/weather.py
+++ helpers/weather.py
@@ -1,145 +1,81 @@
-import json
-from typing import Literal
+from typing import Literal, Dict, List
 
-import requests
+import aiohttp
 
-default_stations = {
-    "KBUF": "Buffalo International Airport",
-    "KDKK": "Dunkirk Airport",
-    "KIAG": "Niagara Falls International Airport",
-    "KJHW": "Chautauqua County/Jamestown Airport",
-}
 loc_station_map = {
     "Buffalo": "KBUF",
     "Niagara Falls": "KIAG",
     "Dunkirk": "KDKK",
     "Jamestown": "KJHW",
 }
-required_times = Literal[
-    "Today",
-    "This Morning",
-    "This Afternoon",
-    "Tonight",
-    "Sunday",
-    "Sunday Night",
-    "Monday",
-    "Monday Night",
-    "Tuesday",
-    "Tuesday Night",
-    "Wednesday",
-    "Wednesday Night",
-    "Thursday",
-    "Thursday Night",
-    "Friday",
-    "Friday Night",
-    "Saturday",
-    "Saturday Night",
-]
+
 locations = Literal["Buffalo", "Niagara Falls", "Dunkirk", "Jamestown"]
-weather_types = Literal[
-    "fog mist",
-    "dust storm",
-    "dust",
-    "drizzle",
-    "funnel cloud",
-    "fog",
-    "smoke",
-    "hail",
-    "snow pellets",
-    "haze",
-    "ice crystals",
-    "ice pellets",
-    "dust whirls",
-    "spray",
-    "rain",
-    "sand",
-    "snow grains",
-    "snow",
-    "squalls",
-    "sand storm",
-    "thunderstorms",
-    "volcanic ash",
-]
-counties = Literal["Erie", "Niagara", "Allegany", "Cattaraugus", "Chautauqua"]
 
 
-async def process_station(interaction, stations, location, weather=None):
-    weather_dict = {}
-    for station in stations:
-        url = f"https://api.weather.gov/stations/{station}/observations/latest"
-        w = requests.get(url)
-        if not w.ok:
-            return await interaction.response.send_message(
+async def get_location_observation(
+    web_client: aiohttp.ClientSession, location: str
+) -> str:
+    station = loc_station_map.get(location)
+    if not station:
+        return f"Unable to find {location} in station mapping. Please report to Admin"
+    url = f"https://api.weather.gov/stations/{station}/observations/latest"
+    async with web_client.get(url) as response:
+        if response.status != 200:
+            return (
                 "The NWS API doesn't want to talk to me right now. " "Try again later"
             )
-        result = json.loads(w.content)["properties"]
-        w.close()
-        # If a location was entered
-        if location:
-            # If a weather type was entered
-            return await process_with_loc(interaction, weather, result, station)
-        # There is no location specified
-        else:
-            # If there weather specified, check each location for it (similar to early iteration of function)
-            if weather:
-                for present_weather in result["presentWeather"]:
-                    if present_weather["weather"] == weather:
-                        weather_dict[default_stations[station]] = present_weather[
-                            "intensity"
-                        ]
-                # If there is no weather in the weather dict, explain that and return out
-                if not weather_dict:
-                    return await interaction.response.send_message(
-                        f"There is no {weather} at any of the local observation stations"
-                    )
-            # No location and no weather specified. Store text descriptions for each location in weatherdict
-            else:
-                weather_dict[default_stations[station]] = result["textDescription"]
-    reply = generate_reply(weather, weather_dict)
-    return await interaction.response.send_message(reply)
+        observation_result = (await response.json())["properties"]
+    temperature = await get_observation_temp(observation_result)
+    url = f"https://api.weather.gov/stations/{station}"
+    async with web_client.get(url) as response:
+        if response.status != 200:
+            return (
+                "The NWS API doesn't want to talk to me right now. " "Try again later"
+            )
+        station_result = (await response.json())["properties"]
+    result = f"Currently it is `{temperature} degrees`"
+    result += (
+        f" and `{observation_result['textDescription']}` "
+        if observation_result["textDescription"]
+        else ""
+    )
+    result += f" at the `{station_result['name']}`"
+    return result
 
 
-async def process_with_loc(interaction, weather, result, station):
-    if weather:
-        # If there is no present weather return out
-        if not result["presentWeather"]:
-            return await interaction.response.send_message(
-                f"There is currently no {weather} at {default_stations[station]}"
-            )
-        # Check for the weather type entered
-        else:
-            # Iterate through present weather list
-            for present_weather in result["presentWeather"]:
-                # If present weather matches entered weather construct reply, send, and return
-                if present_weather == weather:
-                    return await interaction.response.send_message(
-                        "It sure is! Currently there is:\n"
-                        f"{present_weather['intensity']} {weather} "
-                        f"at the {default_stations[station]}"
-                        if present_weather["intensity"]
-                        else f"{weather} at the {default_stations[station]}"
-                    )
-            # This code will be reached if no match to weather is found
-            return await interaction.response.send_message(
-                f"There is currently no {weather} "
-                f"at the {default_stations[station]}"
-            )
-    # There is a location entered, but no weather type
-    else:
-        # Send text description for location
-        return await interaction.response.send_message(
-            f"Currently it is `{result['textDescription']}` "
-            f"at the {default_stations[station]}"
+async def get_observation_temp(observation_result: Dict):
+    fahrenheit = (
+        round(32 + 9 / 5 * observation_result["temperature"]["value"])
+        if observation_result["temperature"]["unitCode"] == "wmoUnit:degC"
+        else observation_result["temperature"]["value"]
+    )
+    return fahrenheit
+
+
+async def generate_forecast_week(periods: List) -> str:
+    forecast_str = "\n".join(
+        [f"`{period['name']}: {period['shortForecast']}`" for period in periods]
+    )
+    return (
+        f"Here is a summary forecast for this week:\n\n{forecast_str}\n\n"
+        f"For a more detailed forecast, pass a time period to `when`. "
+        f"For example: \n`/wx forecast when: Friday`"
+    )
+
+
+async def generate_period_forecast(periods: List, when: str) -> str:
+    times = []
+    result = None
+    for period in periods:
+        if when.lower() != period["name"].lower():
+            times.append(period["name"])
+            continue
+        result = period["detailedForecast"]
+        break
+    if not result:
+        return (
+            f"Could not find `{when}` in time periods. Try one of the following:\n"
+            f"```\n{', '.join([f'{time}' for time in times])}```\n"
+            f".. or leave empty for a summary forecast for this week"
         )
-
-
-def generate_reply(weather, weather_dict):
-    if weather and weather_dict:
-        reply = "It sure is! Currently there is:\n"
-        for k, v in weather_dict.items():
-            reply += f"`{v} {weather} at the {k}`\n" if v else f"{weather} at the {k}\n"
-    else:
-        reply = "Here are the current weather conditions:\n"
-        for k, v in weather_dict.items():
-            reply += f"`{v} at the {k}`\n"
-    return reply
+    return f"Here is a detailed forecast for `{when}`:\n\n" f"`{result}`"
--- startup_cogs/buf_commands.py
+++ startup_cogs/buf_commands.py
@@ -1,85 +1,59 @@
-import requests
-import json
-import calendar
-from datetime import date
 import discord
 from discord.ext import commands
 from discord import app_commands
 
 from helpers.role_select import RoleView
 from helpers.weather import (
-    default_stations,
-    loc_station_map,
-    required_times,
     locations,
-    weather_types,
-    process_station,
+    get_location_observation,
+    generate_forecast_week,
+    generate_period_forecast,
 )
+from buffalobot import BuffaloBot
 
 
 class BufCommands(commands.Cog, name="Buffalo Commands"):
-    def __init__(self, bot) -> None:
+    def __init__(self, bot: BuffaloBot) -> None:
         self.bot = bot
 
     wx = app_commands.Group(name="wx", description="Inquire about WNY weather")
 
     @wx.command(name="current")
-    @app_commands.describe(condition="A weather condition to search the area for")
     @app_commands.describe(
         location="A location with an observation station to search for"
     )
     async def current(
         self,
         interaction: discord.Interaction,
-        condition: weather_types = None,
-        location: locations = None,
+        location: locations,
     ):
-        """Get current weather conditions"""
-        if location:
-            stations = [loc_station_map[location]]
-        else:
-            stations = default_stations
-        return await process_station(interaction, stations, location, condition)
+        """
+        Get live conditions from an official NWS observation station in WNY
+        """
+        return await interaction.response.send_message(
+            await get_location_observation(self.bot.web_client, location)
+        )
 
     @wx.command(name="forecast")
     @app_commands.describe(when="Time period")
-    async def forecast(
-        self, interaction: discord.Interaction, when: required_times = None
-    ):
+    async def forecast(self, interaction: discord.Interaction, when: str = None):
         """Get a quick summary of the next weeks forecast or pass a time period argument to retrieve a more
         detailed forecast of that time period"""
         url = "https://api.weather.gov/gridpoints/BUF/78,43/forecast"
-        w = requests.get(url)
-        if not w.ok:
+        async with self.bot.web_client.get(url) as response:
+            if response.status != 200:
+                return await interaction.response.send_message(
+                    "The NWS API doesn't want to talk to me right now. "
+                    "Try again later"
+                )
+            periods = (await response.json())["properties"]["periods"]
+        if not when:
             return await interaction.response.send_message(
-                "The NWS API doesn't want to talk to me right now. " "Try again later"
+                await generate_forecast_week(periods)
             )
-        result = json.loads(w.content)["properties"]
-        periods = result["periods"]
-        w.close()
-        forecast = ""
-        if when:
-            forecast_type = "detailedForecast"
-            if (
-                when.lower() == "today"
-                or calendar.day_name[date.today().weekday()].lower() in when.lower()
-            ):
-                when_list = ["Today", "This Morning", "This Afternoon", "Tonight"]
-            else:
-                when_list = [when]
-            for period in periods:
-                if period["name"].lower() in [x.lower() for x in when_list]:
-                    forecast += f"`{period['name']}: {period[forecast_type]}`\n"
-            return await interaction.response.send_message(
-                f"Here is a detailed forecast for `{when}`:\n" f"{forecast}"
-            )
-        else:
-            forecast_type = "shortForecast"
-            for period in periods:
-                forecast += f"`{period['name']}: {period[forecast_type]}`\n"
-            return await interaction.response.send_message(
-                f"Here is a summary forecast for this week:\n" f"{forecast}"
-            )
+        return await interaction.response.send_message(
+            await generate_period_forecast(periods, when)
+        )
 
     municipality = app_commands.Group(
         name="municipality",
@@ -95,5 +69,5 @@ class BufCommands(commands.Cog, name="Buffalo Commands"):
         )
 
 
-async def setup(bot: commands.Bot) -> None:
+async def setup(bot: BuffaloBot) -> None:
     await bot.add_cog(BufCommands(bot))
