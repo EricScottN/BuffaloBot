347837b0d93dcd8b00236c21d684aabbb7e25f66
EricScottN <eric.s.northup@gmail.com>
2024-05-24 21:54:37 -0400

    This will run at midnight now

--- startup_cogs/discord_db.py
+++ startup_cogs/discord_db.py
@@ -1,7 +1,7 @@
 """
 Module to initialize database
 """
-
+import datetime
 import logging
 from discord.ext import tasks, commands
 from db.utils import refresh_db
@@ -9,6 +9,9 @@ from db.utils import refresh_db
 from buffalobot import BuffaloBot
 
 logger = logging.getLogger(__name__)
+
+utc = datetime.timezone.utc
+time = datetime.time(hour=0, minute=0, tzinfo=utc)
 
 
 class Database(commands.Cog):
@@ -26,7 +29,7 @@ class Database(commands.Cog):
         """
         self.refresh_db.stop()
 
-    @tasks.loop(hours=24)
+    @tasks.loop(time=time)
     async def refresh_db(self):
         """
         Discord.py task to refresh database on init and every 24 hours
